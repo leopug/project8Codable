35c54235bf79630d7183c565183cf21b01840fa9
leonardo maia pugliese <leo.pug@hotmail.com>
2020-01-03 13:17:29 -0300

    first commit

---
+++ Project8Codable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project8Codable
+//
+//  Created by Ana Caroline de Souza on 03/01/20.
+//  Copyright © 2020 Ana e Leo Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
